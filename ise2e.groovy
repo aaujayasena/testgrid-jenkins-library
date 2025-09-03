626a1ace0488014e987f6ab198c3cd4b9a6b49bb
Achini Jayasena <aaujayasena@gmail.com>
2025-09-03 10:31:56 +0530

    Update script

--- ise2e.groovy
+++ ise2e.groovy
@@ -29,7 +29,7 @@ stages {
         steps {
             script {
                 cfn_repo_url="https://github.com/aaujayasena/testgrid.git" 
-                cfn_repo_branch="e2ecloudformation"
+                cfn_repo_branch="master"
                 if (pre_release.toBoolean()){
                     updateType="pre-release"
                 }
